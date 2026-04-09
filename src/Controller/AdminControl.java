f6bcebe8425cf04696ed7d577e6dd744440e6c4e
Ath07 <Ath07@DESKTOP-V5BIE6N>
2026-04-09 17:29:08 +0700

    REPO II -bug Fixes, Design changes, added assets
    ###################################
    WARNING, MUST INSTALL JafaFX SDK 26
    ###################################
    
    Commit by .theo at 09/04/2026 17:28

--- src/Controller/AdminControl.java
+++ src/Controller/AdminControl.java
@@ -13,10 +13,39 @@ import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 /**
  *
  * @author Ath07
  */
 public class AdminControl {
+    @FXML private AnchorPane PanelDaftar, PanelTambah, PanelRemove, PanelRiwayat;
+
+// Sembunyikan semua, tampilkan yang dipilih
+    private void tampilPane(AnchorPane aktif) {
+    PanelDaftar.setVisible(false);
+    PanelTambah.setVisible(false);
+    PanelRemove.setVisible(false);
+    PanelRiwayat.setVisible(false);
+    aktif.setVisible(true);
+}
+
+    @FXML void btnLookVehicle() { tampilPane(PanelDaftar); }
+    @FXML void btnAddVehicle() { tampilPane(PanelTambah); }
+    @FXML void btnRemoveVehicle() { tampilPane(PanelRemove); }
+    @FXML void btnRiwayatTransaksi() { tampilPane(PanelRiwayat); }
+    
+    @FXML
+    void btnLogout(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/viewForm/login.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     
 }
