3423a60c485d9c6b7040788c9e2dcde1de67df65
Elition1 <gabutbetulan77@gmail.com>
2026-04-10 23:31:46 +0700

    Added logic in UserControl.java. Added object sharing between usercontrol and SistemRental m

--- src/Controller/UserControl.java
+++ src/Controller/UserControl.java
@@ -1,13 +1,91 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controller;
 
-/**
- *
- * @author Ath07
- */
-public class UserControl {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import kendaraanPackage.*;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.fxml.Initializable;
+
+public class UserControl implements Initializable
+{
+    @FXML private AnchorPane LayerUser, LayerBeranda, Layerkendaraan, IsiTabMotor ;
+    @FXML private HBox LayerPesan;
+
+    @FXML private Button TombolBeranda, TombolKendaraan, TombolLogout, ButtonSewaPesanan;
+    @FXML private ToggleButton ButtonLogoHonda, ButtonLogoSuzuki, ButtonLogoKawasaki, ButtonLogoBMW, ButtonLogoMitsubisi, ButtonLogoHondaMotor, ButtonLogoSuzukiMotor, ButtonAll;
+
+    @FXML private ComboBox<Kendaraan> cmbUserCars;
+    @FXML private Tab TabMobil, TabMotor;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        tampilLayar(LayerBeranda);
+
+        SistemRental sisRental = SistemRental.getInstance();
+        Kendaraan[] all = sisRental.semuaKendaraan();
+
+        ObservableList<Kendaraan> list = FXCollections.observableArrayList();
+        for (Kendaraan l : all)
+            if (l != null) list.add(l);
+    }
+
+    private void tampilLayar(AnchorPane aktif){
+        LayerUser.setVisible(false);
+        LayerBeranda.setVisible(false);
+        Layerkendaraan.setVisible(false);
+        aktif.setVisible(true);
+    }
+
+    private void tampilHpane(HBox aktif){
+        LayerPesan.setVisible(false);
+        aktif.setVisible(true);
+    }
+
+    @FXML
+    public void setList(ObservableList<Kendaraan> list)
+    {
+        cmbUserCars.setItems(list);
+    }
+
+    @FXML
+    private void btnSewaLayar() {
+        tampilHpane(LayerPesan);
+    }
+
+    @FXML
+    void btnLogout(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/viewForm/login.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Peringatan(String judul, String pesan) {
+        Alert warning = new Alert(Alert.AlertType.INFORMATION);
+        warning.setTitle(judul);
+        warning.setHeaderText(null);
+        warning.setContentText(pesan);
+        warning.showAndWait();
+    }
 }
